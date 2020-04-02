1e7666144a602273cb3188a5364ae7585b2337c3
sourav110 <sourav.csv@gmail.com>
2020-04-02 11:33:31 +0600

    razor implementation

---
+++ BookRazor/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookRazor.Controllers
+{
+    [Route("api/book")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Books.ToList() });
+        }
+    }
+}
---
+++ BookRazor/Migrations/20200401210759_AddISBN.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookRazor.Migrations
+{
+    public partial class AddISBN : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Books");
+        }
+    }
+}
